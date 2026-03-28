47f1e689b535e46bbfb3072ea7a1b9bd5bab9da6
Hamza-Qayyum <ht86726@gmail.com>
2026-03-28 16:00:29 +0500

    Add Naat Functionality in NaatKhuwaanWebApp project

---
+++ NaatsWebApp/Models/Naat.cs
@@ -0,0 +1,12 @@
+﻿namespace NaatsWebApp.Models
+{
+    public class Naat
+    {
+        public string? nkid { get; set; }
+        public int ano { get; set; }
+        public int nno { get; set; }
+        public string? naattitle { get; set; }
+        public IFormFile? naatfile { get; set; }
+        public string naatpath { get; set; }
+    }
+}
